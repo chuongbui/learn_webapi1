173cae6d58f6e58978d905f877ecd1e2584d13b9
Chuong Bui <chuong.bui@ntq-solution.com.vn>
2017-08-01 10:01:00 +0700

    gen detail

--- MyProject2.Client.Web/Spa/routes.ts
+++ MyProject2.Client.Web/Spa/routes.ts
@@ -3,5 +3,6 @@ export var Routes =
 [
     { route: ['home', ''], moduleId: 'Spa/Views/home', title: 'Home', nav: true },
     { route: 'help', moduleId: 'Spa/Views/help', nav: false },
+    { route: 'CarsObject(/:Id)', moduleId: 'Spa/Views/_CarsObjectView', title: 'Cars Object', nav: false },
     // GENPOINT - generated code will be inserted here. DO NOT REMOVE this line!
 ];
